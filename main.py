5ba10ad2d9c65a4f8a4e2c9c1b6fe3a312155d66
Klay Clarke <klayaclarke@gmail.com>
2021-12-14 00:04:39 -0500

    create login form using Flaskform and wtf

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+import os
+from flask_wtf import FlaskForm
 from flask import Flask, render_template
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+
+app.secret_key = os.environ.get('FLASK_SECRET_KEY')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = StringField('password', validators=[DataRequired()])
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
