de4c77a648e0529af7997ae8480bfd7faabe1116
Klay Clarke <klayaclarke@gmail.com>
2021-12-14 00:12:19 -0500

    add login button to form

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from flask_wtf import FlaskForm
 from flask import Flask, render_template
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -11,7 +11,8 @@ app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    login_button = SubmitField('Login')
 
 
 @app.route("/")
